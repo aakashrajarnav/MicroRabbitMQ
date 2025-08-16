7e1bc5d9b448f2d91456d0abf8e5ff43ce661ab0
Aakash <aakashrajarnav@gmail.com>
2025-08-16 22:57:47 +0530

    Banking Microservices

---
+++ MicroRabbitMQ.Banking.Api/BankingController.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbitMQ.Banking.Application.Interfaces;
+using MicroRabbitMQ.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbitMQ.Banking.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET: api/Banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            var accounts = _accountService.GetAccounts();
+            return Ok(accounts);
+        }
+    }
+}
